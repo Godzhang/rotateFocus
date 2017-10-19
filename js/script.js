673122cc1ba996489272a9a0ba4b7748b453ad29
godzhang <zhangqijilindaxue@126.com>
2017-10-19 22:50:27 +0800

    go on done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-(function(){
+;(function(){
 	var RotateFocus = function(options){
 		this.opt = Object.assign({}, options);
 		this.focus = this.opt.focus;
@@ -21,6 +21,9 @@
 	RotateFocus.prototype = {
 		init: function(){
 			var self = this;
+
+			this.focus.style.width = this.width + "px";
+			this.focus.style.height = this.height + "px";
 
 			this.setFirstFrame();
 			this.setOtherFrame();
@@ -45,15 +48,33 @@
 				
 			}
 			if(dir === 'right'){
+				this.li.forEach(function(cur, index){
+					var suo = index === self.len - 1 ? 0 : ++index,
+						width = self.li[suo].offsetWidth,
+						height = self.li[suo].offsetHeight,
+						top = self.li[suo].offsetTop,
+						left = self.li[suo].offsetLeft,
+						zIndex = self.li[suo].style.zIndex;
 
+					if(suo === 0){
+						width = self.carrouselWidth;
+						height = self.carrouselHeight;
+						top = 0;
+						left = (self.width - self.carrouselWidth) / 2;
+						zIndex = 1000;
+					}
+					cur.style.width = width + "px";
+					cur.style.height = height + "px";
+					cur.style.top = top + "px";
+					cur.style.left = left + "px";
+					cur.style.zIndex = zIndex;
+					self.rotateFlag = true;
+				});
 			}
 		},
 		setFirstFrame: function(){
 			var self = this;
 			var firstFrame = this.li[0];
-
-			this.focus.style.width = this.width + "px";
-			this.focus.style.height = this.height + "px";
 
 			firstFrame.style.zIndex = 1000;
 			firstFrame.style.width = this.carrouselWidth + "px";
@@ -98,7 +119,7 @@
 				val.style.width = rightWidth + "px";
 				val.style.height = rightHeight + "px";
 				val.style.top = (self.height - rightHeight) / 2 + "px";
-				val.style.right = btnWidth - gap * j + "px";
+				val.style.left = btnWidth + self.carrouselWidth + j * gap - rightWidth + "px";
 				val.style.zIndex = zLoop1++;
 				j--;
 			});
